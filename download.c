d10a9837cace29a001452aeb642c96987ff71a91
Bryce Lee <blee42@student.sierracollege.edu>
2024-12-01 15:02:39 -0800

    connection to server commit

---
+++ download.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libsocket/libinetsocket.h>
+
+int main(int argc, char *argv[])
+{
+    char hostname[100];
+    if (argc != 2)
+    {
+        printf("Please enter either london or newark.");
+        exit(1);
+    }
+
+    if (strcmp(argv[1], "london") == 0)
+    {
+         strcpy(hostname, "london.cs.sierracollege.edu");
+    }
+    else if (strcmp(argv[1], "newark") == 0)
+    {
+         strcpy(hostname, "newark.cs.sierracollege.edu");
+    }
+    else
+    {
+        printf("Please enter either london or newark.");
+        exit(1);
+    }
+
+    int fd = create_inet_stream_socket(hostname, "3456", LIBSOCKET_IPv4, 0);
+
+    if (fd<0)
+    {
+        printf("Failed to connect to server.\n");
+        exit(1);
+    }
+
+    printf("Connected to %s\n", hostname);
+}
